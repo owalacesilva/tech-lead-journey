85c3f9868405874229f2c91ac38badfd6e7f49e9
Walace Silva <wsilva.emp@gmail.com>
2024-02-15 18:13:54 -0300

    feat: added sql script init

--- .docker/db-init.sql
+++ .docker/db-init.sql
@@ -1,26 +1,59 @@
 /**
-  * Create a new user and database for the application
-  * This script is only run once when the database container is created
+  * Criar usuário para acessar o banco de dados de serviço de usuário
   */
-CREATE USER developer with encrypted password '123456';
+CREATE USER team_user_service with encrypted password '123456' CREATEDB;
 
 /**
-  * Create a new database for the application
-  * This script is only run once when the database container is created
+  * Criar usuário para acessar o banco de dados de serviço de produto
   */
-CREATE DATABASE service_user;
-GRANT ALL PRIVILEGES ON DATABASE service_user TO developer;
+CREATE USER team_product_service with encrypted password '123456' CREATEDB;
 
 /**
-  * Create a new database for the application
-  * This script is only run once when the database container is created
+  * Criar usuário para acessar o banco de dados de serviço de leiloes
   */
-CREATE DATABASE service_product;
-GRANT ALL PRIVILEGES ON DATABASE service_product TO developer;
+CREATE USER team_auction_service with encrypted password '123456' CREATEDB;
 
 /**
-  * Create a new database for the application
-  * This script is only run once when the database container is created
+  * Criar usuário para acessar o banco de dados de serviço de lances
   */
-CREATE DATABASE service_auction;
-GRANT ALL PRIVILEGES ON DATABASE service_auction TO developer;
+CREATE USER team_bid_service with encrypted password '123456' CREATEDB;
+
+/**
+  * Criar usuário para acessar o banco de dados de serviço de mail
+  */
+CREATE USER team_mail_service with encrypted password '123456' CREATEDB;
+
+/**
+  * Criar usuário para executar apenas migrations no banco de dados
+  */
+CREATE USER team_migration with encrypted password '123456' CREATEDB;
+
+/**
+  * Base de dados para o serviço de usuário
+  */
+CREATE DATABASE user_service OWNER team_user_service;
+GRANT ALL PRIVILEGES ON DATABASE user_service TO team_migration;
+
+/**
+  * Base de dados para o serviço de produto
+  */
+CREATE DATABASE product_service OWNER team_product_service;
+GRANT ALL PRIVILEGES ON DATABASE product_service TO team_migration;
+
+/**
+  * Base de dados para o serviço de leiloes
+  */
+CREATE DATABASE auction_service OWNER team_auction_service;
+GRANT ALL PRIVILEGES ON DATABASE auction_service TO team_migration;
+
+/**
+  * Base de dados para o serviço de lances
+  */
+CREATE DATABASE bid_service OWNER team_bid_service;
+GRANT ALL PRIVILEGES ON DATABASE bid_service TO team_migration;
+
+/**
+  * Base de dados para o serviço de mail
+  */
+CREATE DATABASE mail_service OWNER team_mail_service;
+GRANT ALL PRIVILEGES ON DATABASE mail_service TO team_migration;
