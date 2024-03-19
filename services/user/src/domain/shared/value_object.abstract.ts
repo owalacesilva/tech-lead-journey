f04fef94eee54db5081895445d62c7c6b89a65cc
Walace Silva <wsilva.emp@gmail.com>
2024-03-19 15:11:02 -0300

    refactor: refactored user service

---
+++ services/user/src/domain/shared/value_object.abstract.ts
@@ -0,0 +1,28 @@
+import { IEquatable } from './shared.interfaces';
+
+/**
+ * @description Classe abstrata para objetos de valor, ela
+ * implementa a interface IEquatable
+ * @class ValueObject
+ */
+export abstract class ValueObject<T> implements IEquatable<ValueObject<T>> {
+  protected readonly _props: T;
+
+  public abstract get values(): T;
+
+  constructor(props: T) {
+    this._props = props;
+  }
+
+  public equals(other?: ValueObject<T> | undefined): boolean {
+    if (other === null || other === undefined) {
+      return false;
+    }
+
+    if (other.values === undefined) {
+      return false;
+    }
+
+    return JSON.stringify(this.values) === JSON.stringify(other.values);
+  }
+}
